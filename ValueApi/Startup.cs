418bfb354821784dda5c947537d27ec042c0d3f9
amohammed <amohammed@socketworksng.com>
2022-12-07 20:03:49 +0100

    update start_up_cs

--- ValueApi/Startup.cs
+++ ValueApi/Startup.cs
@@ -23,7 +23,8 @@ namespace ValueApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+          //  services.AddMvc();
+		      services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
